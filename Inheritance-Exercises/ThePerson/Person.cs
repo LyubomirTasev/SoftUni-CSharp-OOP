064f00839db051425fba9e471a1270e7f438771f
Lyubomir Tasev <lubomir.tasev@outlook.com>
2022-02-18 11:55:48 +0200

    Updated Person

---
+++ Inheritance-Exercises/ThePerson/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThePerson
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public int Age
+        {
+            get => age;
+            set => age = value;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"Name: {Name}, Age: {Age}");
+            return sb.ToString();
+        }
+    }
+}
---
+++ Inheritance-Exercises/ThePerson/Program.cs
@@ -0,0 +1,7 @@
+﻿using ThePerson;
+
+string name = Console.ReadLine();
+int age = int.Parse(Console.ReadLine());
+
+Child child = new Child(name, age);
+Console.WriteLine(child);
